70dd245a60392fb67db210eefe83ca8113ced3c2
mrparkersson <amoparkerprince78@gmail.com>
2022-07-24 12:07:39 +0000

    Add router function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const planetRouter = require('./routes/planets/planets.router');
 
 const app = express();
 app.use(express.json());
+app.use(planetRouter);
 
 module.exports = app;
