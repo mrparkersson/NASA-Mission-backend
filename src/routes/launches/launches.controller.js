0ddbdc7d345a29b8f310a3601213bd89542a5a41
mrparkersson <amoparkerprince78@gmail.com>
2022-07-25 13:02:23 +0000

    Add launches controller to get lauch model

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -1,7 +1,7 @@
 const { launches } = require('../../models/launch.model');
 
 function getAllLaunches(req, res) {
-  return res.status(200).json(launches);
+  return res.status(200).json(Array.from(launches.values()));
 }
 
 module.exports = { getAllLaunches };
