5fcc0274ef0f9cf295760c5091534c8fbce30faa
mrparkersson <amoparkerprince78@gmail.com>
2022-07-26 08:49:45 +0000

    Add validation to launch

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -1,7 +1,29 @@
-const { launches } = require('../../models/launch.model');
+const { addNewLaunch, getAllLaunches } = require('../../models/launch.model');
 
-function getAllLaunches(req, res) {
-  return res.status(200).json(Array.from(launches.values()));
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(getAllLaunches());
 }
 
-module.exports = { getAllLaunches };
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+  if (
+    !launch.mission ||
+    !launch.launchDate ||
+    !launch.rocket ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: 'Missing required launch property',
+    });
+  }
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: 'Invalid Date',
+    });
+  }
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
+module.exports = { httpGetAllLaunches, httpAddNewLaunch };
